19d4c44a3d1fc53a8c4086e60711cdf32a5f42cb
Arnaud Lenglet <arlenglet@gmail.com>
2021-11-07 22:33:54 +0100

    feat(fan): create basic start_fan script

---
+++ start_fans.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+transistorPin = 23
+sleep = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(transistorPin, GPIO.OUT)
+
+GPIO.output(transistorPin, GPIO.LOW)
+
+print("Starting fans!")
+GPIO.output(transistorPin, GPIO.HIGH)
