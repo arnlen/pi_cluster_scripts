942c26cacf8d0057d5b7c5f1d11434894727c9db
Arnaud Lenglet <arlenglet@gmail.com>
2021-11-07 23:49:48 +0100

    refactor(monitoring): add time output + more debug outputs

--- start_monitoring.sh
+++ start_monitoring.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-echo "Temperature monitoring started. Hit CTRL+C to stop."
+piName=$PI_NAME
+sleepTime=$MONITORING_SLEEP_TIME
+serverName='pi-cooler'
+apiEndpoint="http://$serverName:3000/temperatures.json"
+
+echo "Temperature monitoring started for $piName. Hit CTRL+C to stop."
+echo "Checking temperature every $sleepTime seconds."
+echo "API Endpoint: $apiEndpoint"
+echo "---"
 
 for (( ; ; ))
 do
   vcgencmdOutput=$(vcgencmd measure_temp)
   reading=${vcgencmdOutput:5:4}
-  piName=$PI_NAME
 
-  serverName='pi-cooler'
-  apiEndpoint='http://'$serverName':3000/temperatures.json'
+  time=$(date +"%T")
+  echo "$time ["$piName"] Temperature: "$reading
 
-  echo "["$piName"] Temperature: "$reading
   curl -X POST -H "Content-Type: application/json" \
     -d '{ "temperature": { "pi_name": "'$piName'", "reading": '$reading' } }' \
     $apiEndpoint
 
-  sleep 30
+  sleep sleepTime
 done
